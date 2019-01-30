65e887dccf7999dc56545d6e96a8c3757d31971e
Daniel J Trifaro <djtrifaro@gmail.com>
2019-01-30 17:11:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function logShout(string) {
 logShout("hello")
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase())) return("I can't hear you!");
+  if (string === string.toLowerCase()) return("I can't hear you!");
   else if (string === string.toUpperCase()) return ("YES INDEED!");
   else if (string === "I love you, Grandma.") return ("I love you, too");
 }
